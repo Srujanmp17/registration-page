41daf534eb9ec4fa0c4e54af17e4bd0d0018e5ce
Srujanmp17 <srujanmp3@gmail.com>
2024-09-22 22:47:00 +0530

    my login page till now

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import *
+app=Flask(__name__)
+
+@app.route('/reg',methods=['POST','GET'])
+def register():
+    print(request.method)
+    if request.method=="POST":
+         name=request.form['name']
+         age=request.form['age']
+         email=request.form['email']
+         phone=request.form['phone']
+         father_name=request.form['father_name']
+         mother_name=request.form['mother_name']
+         course=request.form['course']
+         return redirect(url_for('done'))
+    else:
+         return render_template('register.html')
+         
+@app.route('/sucess')
+def done():
+     return render_template('done.html')
+
+if __name__=="__main__":
+     app.run(debug=True)
+         
